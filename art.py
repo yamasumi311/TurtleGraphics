d690c74f55285cf5e3639f866093a8f0774799da
yamasumi311 <chim.0.don@gmail.com>
2023-03-13 16:35:36 +1100

    added colors on the hexagons

--- art.py
+++ art.py
@@ -2,13 +2,14 @@
 import turtle
 shelly = turtle.Turtle()
 # pick order of colors for the hexagon
-
+colors = ['red', 'yellow', 'blue', 'orange', 'green', 'red']
 # turn background black
 turtle.bgcolor('black')
-
+# make 36 hexagons, each 10 degrees apart
 for n in range(36):
     # repeat 6 times - move forward and turn
     for i in range(6):
+        shelly.color(colors[i])
         shelly.forward(100)
         shelly.left(60)
     # add a turn
