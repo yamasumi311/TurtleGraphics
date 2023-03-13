650b437bfc1dd07fdb582ad14325dbfdcec21771
yamasumi311 <chim.0.don@gmail.com>
2023-03-14 09:55:59 +1100

    added colors and circle size becomes bigger and bigger

--- circleOfCircles.py
+++ circleOfCircles.py
@@ -1,13 +1,17 @@
 import turtle
 
 shelly = turtle.Turtle()
-
+turtle.bgcolor('black')
+colors = ['white', 'green', 'orange', 'blue', 'yellow', 'red']
 for i in range(36):
     shelly.penup()
     shelly.forward(100)
     for n in range(6):
         shelly.pendown()
-        shelly.circle(5)
+        # add colors
+        shelly.color(colors[n])
+        # circles become bigger
+        shelly.circle(5+n)
         shelly.penup()
         shelly.forward(20)
     shelly.goto(0,0)
