2e44fae7dee3f0d77639afe96c88a3b81bbbcf61
yamasumi311 <chim.0.don@gmail.com>
2023-03-13 17:03:20 +1100

    added circle to the hexagons

--- art.py
+++ art.py
@@ -14,4 +14,17 @@ for n in range(36):
         shelly.left(60)
     # add a turn
     shelly.left(10)
+# get ready to draw 36 circles
+shelly.penup()
+shelly.color('white')
+# repeat 36 times to match the 36 hexagons
+for i in range(36):
+    shelly.forward(220)
+    shelly.pendown()
+    shelly.circle(5)
+    shelly.penup()
+    shelly.backward(220)
+    shelly.right(10)
+# hide turtle to finish the drowing
+shelly.hideturtle()
 turtle.done()
